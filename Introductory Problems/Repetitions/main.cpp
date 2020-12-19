b0da6908a19b6763810f430b884dd04db8afdcd6
EdPeReg <dardo1818@gmail.com>
2020-12-19 14:48:06 -0600

    Try 4

--- Introductory Problems/Repetitions/main.cpp
+++ Introductory Problems/Repetitions/main.cpp
@@ -11,17 +11,21 @@ int main() {
 	std::cin >> dna;
 
 	int count = 1;
-	for(size_t i = 0; i < dna.size()-1; ++i) {
-		if(dna.at(i) == dna.at(i+1)) {
-			++count;
-			nums.push_back(count);
-		} else {
-			count = 1;
-			nums.push_back(count);
+	if(dna.size() == 1) {
+		std::cout << 1;
+	} else {
+		for(size_t i = 0; i < dna.size()-1; ++i) {
+			if(dna.at(i) == dna.at(i+1)) {
+				++count;
+				nums.push_back(count);
+			} else {
+				count = 1;
+				nums.push_back(count);
+			}
 		}
-	}
 
-	std::cout << *std::max_element(nums.begin(), nums.end());
+		std::cout << *std::max_element(nums.begin(), nums.end());
+	}
 
 	return 0;
 }
