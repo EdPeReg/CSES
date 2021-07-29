53ff873b5bf935a4b26f49dfc099d9b2b04c240e
eduardo perez regin <eduardo@reginprogrammer.huawei.net>
2021-07-28 23:50:55 -0500

    Try 1

---
+++ Introductory Problems/Bit Strings/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    long long n;
+    std::cin >> n;
+
+    long long aux = 1;
+    long long sum = 0;
+    for(int i = 0; i < n; ++i) {
+        sum += aux;
+        aux *= 2;
+    }
+
+    std::cout << sum + 1<< '\n';
+
+    return 0;
+}
