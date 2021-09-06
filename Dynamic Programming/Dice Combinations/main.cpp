7ed8c032c2cd90a16cb765c737a90f49df943d9a
EdPeReg <dardo1818@gmail.com>
2021-09-05 20:09:36 -0500

    Try 2

--- Dynamic Programming/Dice Combinations/main.cpp
+++ Dynamic Programming/Dice Combinations/main.cpp
@@ -1,11 +1,24 @@
-// 9:57 
+// 1:45:58
+// UNFORTUNATELLY I COULDN'T UNDERSTAND VERY WELL THIS PROBLEM.
+// I NEED TO PRACTICE MORE DP.
 
 #include <iostream>
+#include <vector>
 
 int main() {
+    unsigned long mod = 10e9 + 7;
     int n;
     std::cin >> n;
-    std::cout << n * n / 2;
+
+    std::vector<int> comb(n+1, 0);
+    comb[0] = 1;
+
+    for(int i = 1; i <= n; ++i) 
+        for(int j = 1; j <= 6; ++j)
+            if(i - j >= 0)
+                comb[i] = (comb[i] + comb[i-j]) % mod;
+    
+    std::cout << comb[n];
 
     return 0;
 }
--- Dynamic Programming/Dice Combinations/readme.txt
+++ Dynamic Programming/Dice Combinations/readme.txt
@@ -1,1 +1,4 @@
 https://cses.fi/problemset/task/1633
+
+https://www.youtube.com/watch?v=G6Et8daDTHU
+https://www.youtube.com/watch?v=5gd5jptXWAM
